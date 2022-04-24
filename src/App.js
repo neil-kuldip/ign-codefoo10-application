4828dc39e224943bdaeea97c14288f6a098ba3f5
neil-kuldip <neilkuldip2425@gmail.com>
2022-04-23 20:07:37 -0400

    feat: created video player and initialstyling and url state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import Player from "./components/Player";
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [urls, setUrls] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Player />
     </div>
   );
 }
---
+++ src/components/Player.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Player = () => {
+    return (
+        <>
+            <div className="player">
+                <video controls>
+                    <source src="https://assets14.ign.com/videos/zencoder/2022/04/08/640/ad670c0fbf1850f6be3eb8d0f2fd3581-500000-1649435735.mp4" type="video/mp4" />
+                </video>
+            </div>
+            <div className="controls">
+                <button className="play" data-icon="P" aria-label="play pause toggle"/>
+                <button className="stop" data-icon="S" aria-label="stop"/>
+                <div className="title">
+                    <div></div>
+                    <span aria-label="title">00:00</span>
+                </div>
+                <button class="rwd" data-icon="B" aria-label="rewind"/>
+                <button class="fwd" data-icon="F" aria-label="fast forward"/>
+                Hi
+            </div>
+        </>
+    );
+};
+
+export default Player;

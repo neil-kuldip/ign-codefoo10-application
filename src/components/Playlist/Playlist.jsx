8e1be23e53f28f964dcbfdeae3fe5ee4ebd3b02c
neil-kuldip <neilkuldip2425@gmail.com>
2022-04-30 19:20:28 -0400

    feat: timestamps for video playlist imgs

--- src/components/Playlist/Playlist.jsx
+++ src/components/Playlist/Playlist.jsx
@@ -7,10 +7,16 @@ const PlaylistComponent = ({ currVidId, vidData, selectNextVid, currVidDetails }
 
   const playlist = vidData.map((vid) => {
     if (vid.contentId !== currVidId) {
-      // let hours   = Math.floor(vid.metadata.duration / 3600); 
-      // let minutes = Math.floor((vid.metadata.duration - (hours * 3600)) / 60); 
-      // let seconds = vid.metadata.duration - (hours * 3600) - (minutes * 60); 
-      // console.log(vid.metadata.duration, hours, minutes, seconds);
+      let hours   = Math.floor(vid.metadata.duration / 3600); 
+      let minutes = Math.floor((vid.metadata.duration - (hours * 3600)) / 60); 
+      let seconds = vid.metadata.duration - (hours * 3600) - (minutes * 60); 
+
+      let duration;
+      if (minutes < 10) {minutes = "0" + minutes.toString();}
+      if (seconds < 10) {seconds = "0" + seconds.toString();}
+
+      if (hours > 0) {duration = `${hours}:${minutes}:${seconds}`;}
+      else {duration = `${minutes}:${seconds}`;}
       
       return (
         <Box>
@@ -25,6 +31,7 @@ const PlaylistComponent = ({ currVidId, vidData, selectNextVid, currVidDetails }
             onClick={() => selectNextVid(vid)}
           >
             <img src={vid.thumbnails.at(0).url} className="link"/>
+            <span className="link_duration">{duration}</span>
             <Typography variant="h7" sx={{ mx: "1vw", "&:hover": { textDecoration: "underline" } }}>
               {vid.metadata.title}
             </Typography>
